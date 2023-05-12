e2f9a59de11aec095d69e892c2825692e0c40b9e
Playpaper <heartcolorful@gmail.com>
2023-05-12 10:48:02 +0800

    feat: add login and register route & page

--- app.js
+++ app.js
@@ -3,8 +3,34 @@ const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
 
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+
 app.get('/',(req, res) => {
-  res.send(`It's a web app !`)
+  res.render('index')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.render('logout')
 })
 
 app.listen(PORT, () => console.log(`The express server is listening on http://localhost:${PORT}`))
