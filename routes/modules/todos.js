61fb564f802f2aae0c7b3a42e864c5ff999ef538
Playpaper <heartcolorful@gmail.com>
2023-05-13 22:03:21 +0800

    feat: add new one todo

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,11 +3,24 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
 router.get('/:id', (req, res) => {
   console.log('todos')
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(e => console.log(e))
+})
+
+router.post('/new', (req, res) => {
+  return Todo.create({ 
+    name: req.body.name,
+    UserId: req.user.id
+  })
+    .then(() => res.redirect('/'))
     .catch(e => console.log(e))
 })
 
